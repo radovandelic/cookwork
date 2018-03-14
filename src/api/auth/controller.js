6a5e51d219c9d6eab04b73a45e801b164418c254
Radovan Delic <infamouszgbgd@yahoo.com>
2018-03-14 23:21:16 +0100

    fixed update

--- src/api/auth/controller.js
+++ src/api/auth/controller.js
@@ -2,7 +2,6 @@ import { sign } from '../../services/jwt'
 import { success } from '../../services/response/'
 
 export const login = ({ user }, res, next) => {
-  console.log(user)
   sign(user.id)
     .then((token) => ({ token, user: user.view(true) }))
     .then(success(res, 201))
